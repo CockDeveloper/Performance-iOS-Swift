d08d886cfd59f1be0400e5fdc8147504753d4872
Cock.Developer <cock.developer@gmail.com>
2019-07-02 06:41:39 +0700

    Initial Commit

---
+++ Performance-iOS-SwiftTests/Performance_iOS_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Performance_iOS_SwiftTests.swift
+//  Performance-iOS-SwiftTests
+//
+//  Created by Nguyễn Trọng Anh Tuấn on 02/07/2019.
+//  Copyright © 2019 Cock Developer. All rights reserved.
+//
+
+import XCTest
+@testable import Performance_iOS_Swift
+
+class Performance_iOS_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
